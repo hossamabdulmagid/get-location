2aab506c289921983e78321cc831def271bd0a73
= <hosamabdulmaged@gmail.com>
2022-05-09 20:12:56 +0200

    install fb

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import FindMissingNumber from "./components/algorithm/findmissingnumber.component";
-import FindDublicate from "./components/algorithm/findublicate.component";
+
+import GetLocation from "./components/getlocation/getlocation.component";
 
 const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <FindMissingNumber/>
-                <FindDublicate/>
+                <GetLocation/>
             </header>
         </div>
     );
